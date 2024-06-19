114ae7377b09c1006747debbdeeccbade4b0de2e
Justin Hsu <justin.hsu.1019@gmail.com>
2024-06-19 20:41:07 +0800

    feat: Update new prompts

--- src/utils/call_ai.py
+++ src/utils/call_ai.py
@@ -1,5 +1,3 @@
-import json
-
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.gpt_tem import GPT_Template
@@ -7,25 +5,22 @@ from utils.gemini_tem import Gemini_Template
 
 def call_aied(chart, mess):
     prompt = f"""
-【流程】：{chart}
+以下在一段python流程偽代碼，用於處理發貨問題
+{chart}
 
-【通話紀錄】：{mess}
+以下是客服員工和客戶的對話，請仔細分析，並以JSON格式輸出偽代碼流程中的過程變量及變量值，最後輸出流程中的返回值
+{mess}
+"""
+    prompt_mer = f"""
+以下在一段 流程 Mermaid code，用於處理發貨問題
+{chart}
 
-請根據【流程】檢查【通話紀錄】中客服人員是否有依照【流程】來處理客戶的問題
-
-輸出 JSON 格式：
-{{
-    "流程遵守": ""
-}}
-
-如果客服人員完全遵守流程來對話，請輸出 "流程遵守": "pass"
-如果客服人員僅遵守到 3.1 步，請輸出 "流程遵守": "3.1"
-如果客服人員一開始就沒有依照流程，請輸出 "流程遵守": "0"
+以下是客服員工和客戶的對話，請仔細分析，並以JSON格式輸出偽代碼流程中的過程變量及變量值，最後輸出流程中的返回值
+{mess}
 """
     try:
         # res = GPT_Template(prompt)
         res = Gemini_Template(prompt)
-        res = json.loads(res)["流程遵守"]
     except:
         res = "LLM 當掉囉! 請重新發問 >_<"
 
